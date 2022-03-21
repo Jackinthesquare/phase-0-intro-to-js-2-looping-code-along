c0c6603939f41779954e0192d02f0da6acc55758
Jacky Liang <asheriffy@gmail.com>
2022-03-21 12:32:56 -0400

    commit tree de70076330af7d493ac267ee98524930209d8ea3

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+// function writeCards(names) {
+//     let message = [];
+//     for (let i = 0; i < names.length; i++) {
+//        message.push (`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+//     }
+//     return message
+// }
+
+const writeCards = (names) => {
+    let message = [];
+    for (let i = 0; i < names.length; i++) {
+       message[i]  =`Thank you, ${names[i]}, for the wonderful surprise gift!`
+       console.log(message)
+    }
+    return message
+}
+
+const countDown = (num) => {
+        console.log(num)
+        while (num > 0) {
+        num--;
+        console.log(num)
+    }
+}
